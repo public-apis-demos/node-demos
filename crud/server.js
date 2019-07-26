e8e4dcc3301d772386cbdbd2cf5e05b337ce854d
kasiriveni <kasiriveni@gmail.com>
2019-07-26 21:56:05 +0530

    update the code

--- crud/server.js
+++ crud/server.js
@@ -7,13 +7,13 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 
-console.log(db);
+// console.log(db);
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
 
 const subscribersRouter = require("./routes/subscribers");
-app.use("/subscribers", subscribersRouter);
+app.use("/api", subscribersRouter);
 
 app.listen(3000, () => console.log("Server Started"));

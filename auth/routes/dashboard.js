6bffc15bf7c0f37806850a276d279996d9f162c0
kasiriveni <kasiriveni@gmail.com>
2019-07-27 21:52:07 +0530

    update the file

--- auth/routes/dashboard.js
+++ auth/routes/dashboard.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
-const verify = require('./verifyToken');
+const router = require("express").Router();
+const verify = require("./verifyToken");
 
-router.get('/', verify, (req,res) => {
-    res.send(req.user);
+router.get("/", verify, (req, res) => {
+  res.send(req.user);
 });
 
-router.get('/settings', (req,res) => {
-    res.send("ss");
+router.get("/settings", (req, res) => {
+  res.send("my setting info");
 });
 
-router.get('/any',verify, (req, res) => res.send('Hello World!'))
+router.get("/any", verify, (req, res) => res.send("Hello World!"));
 
 module.exports = router;
